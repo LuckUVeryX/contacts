10fa39eeda287832aee853095a306bdb0d0fc87d
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-12 21:10:23 +0800

    creates contact object

---
+++ lib/core/domain/entities/contact.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+
+class Contact extends Equatable {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String phoneNumber;
+  final String emailAddress;
+
+  const Contact({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.phoneNumber,
+    required this.emailAddress,
+  });
+
+  Contact copyWith({
+    int? id,
+    String? firstName,
+    String? lastName,
+    String? phoneNumber,
+    String? emailAddress,
+  }) {
+    return Contact(
+      id: id ?? this.id,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      emailAddress: emailAddress ?? this.emailAddress,
+    );
+  }
+
+  @override
+  List<Object> get props => [firstName, lastName, phoneNumber, emailAddress];
+}
