5da4289099e11cda1312d64426ba751135af1620
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-13 11:35:21 +0800

    adds bloc

--- lib/features/contacts_list/presentation/bloc/contacts_list_event.dart
+++ lib/features/contacts_list/presentation/bloc/contacts_list_event.dart
@@ -4,5 +4,27 @@ abstract class ContactsListEvent extends Equatable {
   const ContactsListEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
+
+class ContactsListSubscriptionRequested extends ContactsListEvent {}
+
+class ContactsListContactSaved extends ContactsListEvent {
+  const ContactsListContactSaved(this.contact);
+
+  final Contact contact;
+
+  @override
+  List<Object?> get props => [contact];
+}
+
+class ContactsListContactDeleted extends ContactsListEvent {
+  const ContactsListContactDeleted(this.contact);
+
+  final Contact contact;
+
+  @override
+  List<Object?> get props => [contact];
+}
+
+class ContactsListUndoDeletionRequested extends ContactsListEvent {}
