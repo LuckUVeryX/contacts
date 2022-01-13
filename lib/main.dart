8d59544fd048979e6a041cfb102d3d1f3a4f2f68
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-13 22:59:23 +0800

    provides contacts repository to the entire application

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
 
+import 'core/data/datasources/contacts_local_datasource.dart';
+import 'core/data/repositories/contacts_repostory_impl.dart';
 import 'core/observers/observers.dart';
 import 'core/router/router.gr.dart';
 import 'core/theme/theme.dart';
 
-void main() {
+Future<void> main() async {
   // Sets the logging level.
   Logger.level = Level.info;
+
+  WidgetsFlutterBinding.ensureInitialized();
+  final repository = ContactsRepository(ContactsLocalDataSource());
+  await repository.init();
+
   BlocOverrides.runZoned(
-    () => runApp(MyApp()),
+    () => runApp(
+      RepositoryProvider(create: (_) => repository, child: MyApp()),
+    ),
     blocObserver: SimpleBlocObserver(),
   );
 }
