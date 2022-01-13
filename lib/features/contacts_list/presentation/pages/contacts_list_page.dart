cae4d3bbe226ba3fc0398c2bcbd3d90293263699
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-13 22:59:37 +0800

    adds bloc contacts list page

--- lib/features/contacts_list/presentation/pages/contacts_list_page.dart
+++ lib/features/contacts_list/presentation/pages/contacts_list_page.dart
@@ -1,28 +1,97 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../core/router/router.gr.dart';
+import '../../../../core/data/repositories/contacts_repostory_impl.dart';
+import '../bloc/contacts_list_bloc.dart';
 
 class ContactsListPage extends StatelessWidget {
   const ContactsListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              AutoRouter.of(context).navigate(const EditContactsRoute());
+    return BlocProvider(
+      create: (context) => ContactsListBloc(context.read<ContactsRepository>())
+        ..add(ContactsListSubscriptionRequested()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Contacts'),
+        ),
+        body: MultiBlocListener(
+          listeners: [
+            BlocListener<ContactsListBloc, ContactsListState>(
+              listenWhen: (prev, curr) => prev.status != curr.status,
+              listener: (context, state) {
+                if (state.status == ContactsListStatus.failure) {
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(
+                      // TODO: Refactor Snackbars
+                      const SnackBar(content: Text('Failed to fetch contacts')),
+                    );
+                }
+              },
+            ),
+            BlocListener<ContactsListBloc, ContactsListState>(
+              listenWhen: (previous, current) =>
+                  previous.lastDeletedContact != current.lastDeletedContact &&
+                  current.lastDeletedContact != null,
+              listener: (context, state) {
+                final deletedContact = state.lastDeletedContact!;
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    // TODO: Refactor Snackbars
+                    SnackBar(
+                      content: Text(
+                        'Deleted Contact ${deletedContact.firstName} ${deletedContact.lastName}',
+                      ),
+                      action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                          context
+                              .read<ContactsListBloc>()
+                              .add(ContactsListUndoDeletionRequested());
+                        },
+                      ),
+                    ),
+                  );
+              },
+            )
+          ],
+          child: BlocBuilder<ContactsListBloc, ContactsListState>(
+            builder: (context, state) {
+              if (state.contacts.isEmpty) {
+                if (state.status == ContactsListStatus.loading) {
+                  return const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+                } else if (state.status != ContactsListStatus.success) {
+                  return const Center(
+                    child: Text('Uh oh, something has went horribly wrong'),
+                  );
+                } else {
+                  // TODO: Add arrow to point to floating action button.
+                  return const Center(
+                    child: Text(
+                        'You have no contacts! Add one by tapping the add button'),
+                  );
+                }
+              }
+
+              return ListView.builder(
+                itemCount: state.contacts.length,
+                itemBuilder: (context, index) {
+                  return Text(state.contacts[index].emailAddress);
+                },
+              );
             },
-            icon: const Icon(Icons.edit),
-          )
-        ],
-      ),
-      body: const Center(child: Text('ContactsListPage')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
       ),
     );
   }
