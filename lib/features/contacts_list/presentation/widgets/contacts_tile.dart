07643b72498e7f6afab44fde72987d9c0e9449a5
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-14 15:45:10 +0800

    minor ui changes and adds navigation triggers

--- lib/features/contacts_list/presentation/widgets/contacts_tile.dart
+++ lib/features/contacts_list/presentation/widgets/contacts_tile.dart
@@ -1,6 +1,8 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/domain/entities/contact.dart';
+import '../../../../core/router/router.gr.dart';
 
 class ContactsTile extends StatelessWidget {
   const ContactsTile({
@@ -16,52 +18,64 @@ class ContactsTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
-    return Dismissible(
-      key: Key(contact.id.toString()),
-      onDismissed: onDismissed,
-      background: Container(
-        color: colorScheme.error,
-        child: Row(
-          children: const [
-            Spacer(),
-            Icon(Icons.delete, color: Colors.white),
-            SizedBox(width: 32.0),
-          ],
-        ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 8.0,
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 48.0,
-              width: 48.0,
-              decoration: BoxDecoration(
-                color: contact.profileColor,
-                borderRadius: BorderRadius.circular(8.0),
-              ),
+    return Card(
+      child: InkWell(
+        onTap: () {
+          AutoRouter.of(context).push(ContactInfoRoute(contact: contact));
+        },
+        child: Dismissible(
+          key: Key(contact.id.toString()),
+          direction: DismissDirection.endToStart,
+          onDismissed: onDismissed,
+          background: Container(
+            color: colorScheme.error,
+            child: Row(
+              children: const [
+                Spacer(),
+                Icon(Icons.delete, color: Colors.white),
+                SizedBox(width: 32.0),
+              ],
             ),
-            const SizedBox(width: 32.0),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    contact.firstName + ' ' + contact.lastName,
-                    style: textTheme.bodyText1,
-                  ),
-                  Text(
-                    contact.phoneNumber,
-                    style: textTheme.caption,
-                  ),
-                ],
-              ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
             ),
-          ],
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 24.0,
+                  backgroundColor: contact.profileColor,
+                ),
+                const SizedBox(width: 32.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        contact.firstName + ' ' + contact.lastName,
+                        style: textTheme.bodyText1,
+                      ),
+                      Text(
+                        contact.phoneNumber,
+                        style: textTheme.caption,
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    AutoRouter.of(context).push(
+                      EditContactsRoute(contact: contact),
+                    );
+                  },
+                  icon: const Icon(Icons.more_vert),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
