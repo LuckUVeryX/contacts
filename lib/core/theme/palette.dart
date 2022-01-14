645a8db8b27883e5f443bf060990e144ca9fdd64
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2022-01-14 16:26:35 +0800

    Feat/profile color purple shades (#20)
    * adds purple shades and randomColor getter
    
    * provide random purple color to raw contacts

--- lib/core/theme/palette.dart
+++ lib/core/theme/palette.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/painting.dart';
 
 class Palette {
@@ -5,4 +7,39 @@ class Palette {
   static const Color accent = Color(0xffeebbc3);
   static const Color headline = Color(0xfffffffe);
   static const Color paragraph = Color(0xffb8c1ec);
+}
+
+class PurpleShades {
+  static const List<Color> colors = [
+    Color(0xFF9F2B68),
+    Color(0xFFBF40BF),
+    Color(0xFF800020),
+    Color(0xFF702963),
+    Color(0xFFAA336A),
+    Color(0xFF301934),
+    Color(0xFF483248),
+    Color(0xFF5D3FD3),
+    Color(0xFFE6E6FA),
+    Color(0xFFCBC3E3),
+    Color(0xFFCF9FFF),
+    Color(0xFFAA98A9),
+    Color(0xFFE0B0FF),
+    Color(0xFF915F6D),
+    Color(0xFF770737),
+    Color(0xFFDA70D6),
+    Color(0xFFC3B1E1),
+    Color(0xFFCCCCFF),
+    Color(0xFF673147),
+    Color(0xFFA95C68),
+    Color(0xFF800080),
+    Color(0xFF51414F),
+    Color(0xFF953553),
+    Color(0xFFD8BFD8),
+    Color(0xFF630330),
+    Color(0xFF7F00FF),
+    Color(0xFF722F37),
+    Color(0xFFBDB5D5),
+  ];
+
+  static Color get randomColor => colors[Random().nextInt(colors.length)];
 }
