0f05121a1cfb1cbaca9dd774d6798328e1daaa0c
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2022-01-15 14:17:55 +0800

    adds sliver app bar

--- lib/features/contact_info/presentation/pages/contact_info_page.dart
+++ lib/features/contact_info/presentation/pages/contact_info_page.dart
@@ -12,6 +12,49 @@ class ContactInfoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(contact.toString())));
+    // return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              centerTitle: true,
+              expandedHeight: 160.0,
+              collapsedHeight: kToolbarHeight + 42,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 8.0),
+                    CircleAvatar(backgroundColor: contact.profileColor),
+                    const SizedBox(height: 8.0),
+                    Text(
+                      contact.firstName + ' ' + contact.lastName,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Container(
+                    color: Colors.red,
+                    height: 80,
+                    width: 10,
+                    margin: EdgeInsets.all(8.0),
+                  );
+                },
+                childCount: 50,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
