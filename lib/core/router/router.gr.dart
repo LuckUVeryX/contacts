4a4a68fe0b908db5000e9757f3e2358f17c8265b
Ryan Yip <71072280+LuckUVeryX@users.noreply.github.com>
2022-01-15 17:21:03 +0800

    Fix/info page not reactive (#25)
    * adds fromContactId function
    
    * uses bloc state to read the contact instead of using a static contact

--- lib/core/router/router.gr.dart
+++ lib/core/router/router.gr.dart
@@ -15,11 +15,13 @@ import 'package:flutter/material.dart' as _i5;
 
 import '../../features/contact_info/presentation/pages/contact_info_page.dart'
     as _i2;
+import '../../features/contacts_list/presentation/bloc/contacts_list_bloc.dart'
+    as _i6;
 import '../../features/contacts_list/presentation/pages/contacts_list_page.dart'
     as _i1;
 import '../../features/edit_contact/presentation/pages/edit_contact_page.dart'
     as _i3;
-import '../domain/entities/contact.dart' as _i6;
+import '../domain/entities/contact.dart' as _i7;
 
 class AppRouter extends _i4.RootStackRouter {
   AppRouter([_i5.GlobalKey<_i5.NavigatorState>? navigatorKey])
@@ -35,7 +37,8 @@ class AppRouter extends _i4.RootStackRouter {
       final args = routeData.argsAs<ContactInfoRouteArgs>();
       return _i4.MaterialPageX<dynamic>(
           routeData: routeData,
-          child: _i2.ContactInfoPage(key: args.key, contact: args.contact));
+          child:
+              _i2.ContactInfoPage(key: args.key, id: args.id, bloc: args.bloc));
     },
     EditContactsRoute.name: (routeData) {
       final args = routeData.argsAs<EditContactsRouteArgs>(
@@ -65,31 +68,34 @@ class ContactsListRoute extends _i4.PageRouteInfo<void> {
 /// generated route for
 /// [_i2.ContactInfoPage]
 class ContactInfoRoute extends _i4.PageRouteInfo<ContactInfoRouteArgs> {
-  ContactInfoRoute({_i5.Key? key, required _i6.Contact contact})
+  ContactInfoRoute(
+      {_i5.Key? key, required int id, required _i6.ContactsListBloc bloc})
       : super(ContactInfoRoute.name,
             path: '/contact-info-page',
-            args: ContactInfoRouteArgs(key: key, contact: contact));
+            args: ContactInfoRouteArgs(key: key, id: id, bloc: bloc));
 
   static const String name = 'ContactInfoRoute';
 }
 
 class ContactInfoRouteArgs {
-  const ContactInfoRouteArgs({this.key, required this.contact});
+  const ContactInfoRouteArgs({this.key, required this.id, required this.bloc});
 
   final _i5.Key? key;
 
-  final _i6.Contact contact;
+  final int id;
+
+  final _i6.ContactsListBloc bloc;
 
   @override
   String toString() {
-    return 'ContactInfoRouteArgs{key: $key, contact: $contact}';
+    return 'ContactInfoRouteArgs{key: $key, id: $id, bloc: $bloc}';
   }
 }
 
 /// generated route for
 /// [_i3.EditContactsPage]
 class EditContactsRoute extends _i4.PageRouteInfo<EditContactsRouteArgs> {
-  EditContactsRoute({_i5.Key? key, _i6.Contact? contact})
+  EditContactsRoute({_i5.Key? key, _i7.Contact? contact})
       : super(EditContactsRoute.name,
             path: '/edit-contacts-page',
             args: EditContactsRouteArgs(key: key, contact: contact));
@@ -102,7 +108,7 @@ class EditContactsRouteArgs {
 
   final _i5.Key? key;
 
-  final _i6.Contact? contact;
+  final _i7.Contact? contact;
 
   @override
   String toString() {
